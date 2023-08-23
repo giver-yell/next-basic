a66e54ac36eb02b59bcbf26772ee31ffbbed86ef
Ryohei Fujihara <giver.yell@gmail.com>
2023-08-24 07:16:34 +0900

    add: render markdown

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,8 +1,9 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export async function getStaticProps({ params}) {
-  const postData = getPostData(params.id);
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData
@@ -27,6 +28,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
